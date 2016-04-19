2c6fba6cc970b2c1592f4c51eca23a41d43dd0a3
sueliopss <sueliopss@gmail.com>
2016-04-19 00:59:06 -0300

    modal new product

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
   //$('#myModal').modal({"backdrop": "static"});
-
+  newProdutoClickListener();
   $("table td a").click(function(){
     $('#myModal').modal({"backdrop": "static"});
   })
 });
+
+var newProdutoClickListener = function(){
+  $("#new_product_button").click(function(){
+    $("#new_product_modal").modal({"backdrop": "static"});
+  });
+}
